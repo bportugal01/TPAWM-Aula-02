e490adec0e1ea778cdc77530883aa8e441b1e2b9
bportugal01 <bruno2004sa@gmail.com>
2024-02-26 20:52:08 -0300

    adicionando rotas

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ app.get("/", function (req, res) {
     res.end("<h1>Minha primeira rota com Express</h1>")
 })
 
-app.get("/cadastrar/:item/:produto/:codigo", function (req, res) {
-    res.send("Item: " + req.params.item + "<br>" + "Produto: " + req.params.produto + "<br>" + "Código: " + req.params.codigo)
-    //res.send("<h1 style='color: #006400;'>Cadastro de usuário</h1>")
+app.get("/cadastrar/:produto", function (req, res) {
+    res.send("Cadastro de produto: " + req.params.produto )
+
+
 })
 
+app.get("/pesquisar/:produto/:modelo", function (req, res) {
+    res.send("Pesquisa de produto: " + req.params.produto +"<br>"+ " Modelo: " + req.params.modelo)
+})
+
+app.get("/contato/:sac/:produto", function (req, res) {
+    res.send("Contato: " + req.params.sac +"<br>"+ " Produto: " + req.params.produto)
+})
 app.listen(8081, function () {
     console.log("A porra do servidor tá rodando na porta 8081!")
 })
